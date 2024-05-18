ffebc81df43e830093bf90c81017f3443a6ce656
johnlee12 <iam@undermilli.com>
2024-05-18 15:19:10 +0900

    feat: ejs + other things + voice chat

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 const server = http.createServer(app);
 
 app.get('/', (req, res) => {
-  res.sendFile(path.resolve(__dirname, 'src', 'index.html'));
+  res.sendFile(path.resolve(__dirname, 'src', 'index-stt.html'));
 });
 
 const checklist = [
@@ -35,9 +35,32 @@ const checklist = [
   "가정 폭력을 경험하고 계신가요? 예를 들어, 가해자가 통제하는 행동을 하거나 신체적인 폭행을 하는 등의 일이 있었는지, 본인이나 자녀에게 위협을 가한 적이 있는지, 그리고 현재 안전 대책이 마련되어 있는지 등에 대해 자세히 설명해 주시겠어요? 특히 당신의 안전이 당장 위험에 처해 있다고 생각되시나요?"
 ];
 
+const promptForComplication = `
+위에서 물어본 질문들을 카테고리화 하면 아래와 같아. 환자의 답변을 기반으로 
+정신건강 문제 (우울, 불안, 정신병적 증상 등)
+의학적 문제 (만성 질환, 통증 등)
+사회적 문제 (실업, 주거 불안정, 가정 폭력 등)
+법적 문제 (보호관찰, 미결 사건 등)
+기타 중독 치료를 어렵게 하는 요인들
+이 5가지 카테고리 중 총 몇 가지가 해당하는 환자인지 답을 내줘.
+font decoration 없이 출력
+`
+
+const finalPrompt = `
+중독과 복잡성 요인 개수에 따라 Tier를 분류할 수 있고 그 기준은 다음과 같아. 해당 환자가 해당하는 tier와 그 근거를 알려줘.
+중독 있음은 AUDIT score > 20 이거나, DUDIT score >24를 뜻 해.
+복잡성 요인이란 위에서 5가지 카테고리 중 몇 개의 카테고리에 속하는지를 뜻 해.
+중독 없음 + 복잡성 요인 없음 = Tier 1
+중독 없음 + 복잡성 요인 있음 = Tier 2
+중독 있음 + 복잡성 요인 0-1개 = Tier 3
+중독 있음 + 복잡성 요인 2-3개 = Tier 4
+중독 있음 + 복잡성 요인 4개 이상 = Tier 5
+
+font decoration 없이 출력
+`
 
 const checklistToBoolean = (conv) => {
-  const list = `checklist: ${checklist.toString()}
+  const list = `checklist: [${checklist.toString()}]
     
     conversation: ${conv}
     
@@ -46,8 +69,9 @@ const checklistToBoolean = (conv) => {
 };
 
 const jsonToPrompt = (conv) => {
-  const list = `${checklist.toString()}  = ${conv}
-  이 내용을 기반으로 의사가 물어볼 다음 질문을 자연스럽게 하나만 뽑아주세요 (질문만 제시), true로 체크된 질문은 제외합니다. checklist 순서대로 출력해주세요.`
+  const list = `${conv}
+  이 내용을 기반으로 의사가 물어볼 다음 질문을 자연스럽게 하나만 뽑아주세요 (질문만 제시), false로 체크된 내용만 질문해주세요. checklist 순서대로 출력해주세요.
+  `
   return list;
 };
 
@@ -61,14 +85,17 @@ const combineArraysToObject = (keys, values) => {
 
 let prevConversation = [];
 
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'ejs'));
+
 app.post("/submit", async (req, res) => {
   const { text } = req.body;
   console.log(text);
-  const openai = new OpenAI({
+  const solar = new OpenAI({
     apiKey: process.env.SOLAR_API_KEY,
     baseURL: 'https://api.upstage.ai/v1/solar'
   })
-  const chatCompletion = await openai.chat.completions.create({
+  const chatCompletion = await solar.chat.completions.create({
     model: 'solar-1-mini-chat',
     messages: [
       {
@@ -76,11 +103,50 @@ app.post("/submit", async (req, res) => {
         content: 'contents: ' + text + '\n contents 내용을 요약해주세요.'
       }
     ],});
-  console.log(chatCompletion.choices[0].message.content);
-  if (chatCompletion && chatCompletion.choices && chatCompletion.choices.length > 0) {
-    res.json({ response: chatCompletion.choices[0].message.content });
+  const abbrev = chatCompletion.choices[0].message.content
+
+  const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY, 
+  });
+
+  const resultText = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: text + promptForComplication,
+      },
+    ],
+    model: "gpt-4o",
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  const complication = resultText.choices[0].message.content;
+
+  const finalResult = await openai.chat.completions.create({
+    messages: [
+      {
+        role: "user",
+        content: `전체 대화 내용: ${text} \n\n complication 점수: ${complication} \n\n ${finalPrompt}`,
+      },
+    ],
+    model: "gpt-4o",
+    temperature: 1,
+    max_tokens: 256,
+    top_p: 1,
+    frequency_penalty: 0,
+    presence_penalty: 0,
+  });
+
+  const final = finalResult.choices[0].message.content;
+
+  if (abbrev) {
+    res.render('index', { response: abbrev, complication, final });
   } else {
-    res.status(500).json({ error: "Invalid response format from OpenAI API" });
+    res.status(500).render('response', { response: "Invalid response format from OpenAI API" });
   }
 });
 
@@ -111,12 +177,11 @@ app.post("/upload-text", async (req, res) => {
 
   const jsonString = resultText.choices[0].message.content.replace(/```json\n|```/g, '');
 
-  const promptAsk = jsonToPrompt(jsonString);
-
-
   const lists = combineArraysToObject(checklist, JSON.parse(jsonString));
 
   console.log(lists)
+
+  const promptAsk = jsonToPrompt(JSON.stringify(lists));
   
   const prompt2 = [
     {role: "user", content: promptAsk,}
@@ -133,7 +198,6 @@ app.post("/upload-text", async (req, res) => {
   });
 
   prevConversation.push({ role: "assistant", content: resultText2.choices[0].message.content });
-
 
   if (resultText2 && resultText2.choices && resultText2.choices.length > 0) {
     res.json({ response: resultText2.choices[0].message.content, lists });
